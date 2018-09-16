5984a5814bfacc2626f0eb15dcfb4e3b57095fd9
David Viejo <davidviejopomata@gmail.com>
2018-09-16 13:28:42 +0200

    Add project files.

---
+++ I18n.Example/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace I18n.Example.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
